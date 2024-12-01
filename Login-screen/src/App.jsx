5f9295c53f156025ecb51f1ba18e7d8decbe70a0
Mokete <ematyabekana@gmail.com>
2024-12-01 15:06:57 +0200

    Created a react template and added bootstrap.

---
+++ Login-screen/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <form action="">
+        <label htmlFor="name">Username</label>
+        <input type="text" name='name'/>
+
+        <label htmlFor="email">Email Address</label>
+        <input type="email" />
+      </form>
+    </div>
+    </>
+  )
+}
+
+export default App
